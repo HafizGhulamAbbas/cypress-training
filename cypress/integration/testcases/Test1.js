51c0e0be0ef359c5efb918f7ead601926f87bda5
HafizGhulamAbas <hgabbbas1122@gmail.com>
2022-10-06 12:25:00 +0500

    Handle invisible elements

--- cypress/integration/testcases/Test1.js
+++ cypress/integration/testcases/Test1.js
@@ -6,6 +6,7 @@ describe('My first test suite', function () {
         cy.visit(Cypress.env('url'))
         cy.get('.search-keyword').type('ca')
         cy.wait(2000)
-        cy.get('.product').should('have.length', 5)
+        cy.get('.product:visible').should('have.length', 4)
+        // cy.get('.products').children().should('have.length', 4)
     })
   })
