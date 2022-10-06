7f7c29ff3d3295cad718b540244a3819aeadf4c2
HafizGhulamAbas <hgabbbas1122@gmail.com>
2022-10-06 12:32:56 +0500

    Parent Child Chaining

--- cypress/integration/testcases/Test1.js
+++ cypress/integration/testcases/Test1.js
@@ -7,6 +7,11 @@ describe('My first test suite', function () {
         cy.get('.search-keyword').type('ca')
         cy.wait(2000)
         cy.get('.product:visible').should('have.length', 4)
-        // cy.get('.products').children().should('have.length', 4)
+
+        // Parent children chaining
+        cy.get('.products').find('.product').should('have.length', 4)
+
+        // Another easy way
+        cy.get('.products').children().should('have.length', 4)
     })
   })
