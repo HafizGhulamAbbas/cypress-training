6b94c5669f63452b7b32eedd30f3de8d40d49036
HafizGhulamAbas <hgabbbas1122@gmail.com>
2022-10-10 12:04:30 +0500

    Verify static response matching a fixture object

--- cypress/integration/testcases/API.js
+++ cypress/integration/testcases/API.js
@@ -1,4 +1,5 @@
 describe('API testing using cypress', () => {
+    
     it('Intercept a request to return one record only', () => {
         cy.visit('https://rahulshettyacademy.com/angularAppdemo/')
 
@@ -6,6 +7,7 @@ describe('API testing using cypress', () => {
             method: 'GET',
             url: 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
         },
+        // []  force the response to be: []
         {
             statusCode: 200,
             body: [{
@@ -33,6 +35,25 @@ describe('API testing using cypress', () => {
         }).as('changedUrlResponse')
         cy.get('button.btn.btn-primary').click()
         cy.wait('@changedUrlResponse')
+    })
+
+    it('gives a response matching a fixture object', () => {
+        // cy.visit('https://rahulshettyacademy.com/angularAppdemo/')
+        // // we set the response to be the activites.json fixture
+        // cy.intercept('GET', 'https://rahulshettyacademy.com/angularAppdemo/', { fixture: 'library.json' }).as('testing')
+        // cy.get('button.btn.btn-primary').click()
+        // cy.wait('@testing').should(({request, response}) => {
+        //     cy.get('tr').should('have.length', response.body.length+1)
+        // })
+        // cy.get('p').should('have.text', 'Oops only 1 Book available')
+
+
+
+        cy.request('https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty')
+        .its('body')
+        .then((body) => {
+            cy.fixture('library.json').should('deep.equal', body)
+        })
     })
 })
 
